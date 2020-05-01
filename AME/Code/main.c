5a96058062a0ee70ece919e73bca32fdf127b7ea
Coder38611 <54740236+coder38611@users.noreply.github.com>
2020-05-02 07:46:40 +0900

    Console Size Set

--- AME/Code/main.c
+++ AME/Code/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 #include <conio.h>
 #include "event_screen.h"
@@ -6,6 +7,7 @@
 
 int main(void) {
 	SetConsoleTitle("AME: 2253 | 안드로이드 미로 탈출: 2253번째 실험");
+	system("mode con cols=67 lines=20");
 	start_screen();
 	return 0;
 }
